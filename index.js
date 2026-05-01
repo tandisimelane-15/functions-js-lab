7153c91267460b7a1f420ceed35d0c1b2cd3aa0f
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-05-01 15:50:24 +0300

    Final changes

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+//functions
 
+function calculateTax(amount){
+    return amount * 0.1
+};
+
+function convertToUpperCase(text){
+    return text.toUpperCase()
+};
+
+function findMaximum(num1, num2){
+    if (num1 > num2){
+        return num1
+    }else{
+        return num2
+    }
+};
+
+function isPalindrome(word){
+    const reversed = word.split('').reverse().join('')
+    if (word === reversed){
+        return true
+    }else{
+        return false
+    }
+};
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    const discount = originalPrice * (discountPercentage / 100)
+    return originalPrice - discount
+};
 
 
 
